316520cfeacd6a9dfd405f6623e96eceffa78781
Sajith <krsajith@gmail.com>
2024-11-07 19:34:48 +0530

    basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as echarts from 'echarts/core';
+import {
+  BarChart,
+  LineChart,
+  PieChart
+} from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent
+} from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+
+// Register the required ECharts components
+echarts.use([
+  BarChart,
+  LineChart,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  CanvasRenderer
+]);
 
 @Component({
   selector: 'app-root',
@@ -8,6 +31,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'echart-sample';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const  option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }
+      ]
+    };
+
+    var chartDom = document.getElementById('chart')!;
+    var myChart = echarts.init(chartDom);
+    option && myChart.setOption(option);
+  }
+
 }
